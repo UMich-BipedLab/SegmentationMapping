3075b1fd527e752ef7e45954547c130ff6f43c8f
zeroAska <trotwoodray@gmail.com>
2019-01-08 20:24:44 +0800

    testing..

--- scripts/projection.py
+++ scripts/projection.py
@@ -1,11 +1,10 @@
-
-import numpy as np
-import tensorflow as tf
 import sys,os
-import tensorflow as tf
+import numpy as np
+#import tensorflow as tf
+import cv2
 
 import rospy
-import cv_bridge
+
 
 class LidarSeg:
     def __init__(self, neural_net_graph_path):
@@ -31,11 +30,8 @@ class LidarSeg:
         self.instrinsic = []
         self.cam2lidar  = []
 
-        # cv_bridge
-        self.bridge = CvBridge()
 
-
-    def add_cam(instrinsic_mat, cam2lidar):
+    def add_cam(self,instrinsic_mat, cam2lidar):
         """
         param: intrinsic_mat: 3x4 
                extrinsic_mat: 4x4, imu2cam
@@ -43,27 +39,41 @@ class LidarSeg:
         self.intrinsic.append(intrinsic_mat)
         self.cam2lidar.append( cam2lidar )
 
-    def project_lidar_to_seg(lidar, rgb_img, camera_ind):
+    def project_lidar_to_seg(self, lidar, rgb_img, camera_ind):
         """
         assume the lidar points can all be projected to this img
+        assume the rgb img shape meets the requirement of the neural net
+
         lidar points: 3xN
         """
         out = self.sess.run(self.y, feed_dict={self.x: rgb_img})
         T_c2l = self.cam2lidar[camera_ind]
         lidar_in_cam = np.matmul(T_c2l, lidar)
-        projected_lidar_2d = np.matmul(self.intrinsic[camera_ind], lidar_in_cam)
+        projected_lidar_2d = np.matmul(self.intrinsic[camera_ind][:, :-1], lidar_in_cam)
+        projected_lidar_2d[:, 0] = projected_lidar_2d[:, 0] / projected_lidar_2d[:,2]
+        projected_lidar_2d[:, 1] = projected_lidar_2d[:, 1] / projected_lidar_2d[:,2]
+        projected_lidar_2d[:, 2] = 1
         labels = np.zeros((1, lidar.shape[1] ))
 
         for col in range(projected_lidar_2d.shape[1]):
             u, v, d = projected_lidar_2d[:, col]
             print("coordinate "+str((u,v,d)))
-            labels[col] =  out[u, v]
+            labels[col] = out[u, v]
 
-
+        self.visualization(labels, projected_lidar_2d, rgb_img)
         return labels
         
 
-    
+    def visualization(self, labels, projected_points, rgb_img):
+        to_show = rgb_img
+        for i in range(labels.shape[1] ):
+            cv2.putText(to_show, str(labels[i]), 
+                        (projected_points[1, i], projected_points[0,i]),
+                        cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
+        cv2.imshow("projected",to_show)
+        cv2.waitKey(0)
+
+        
 
 
     
