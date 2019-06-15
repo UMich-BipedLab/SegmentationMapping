29f89da51fe752925bbedc0831847f4b5da11bd2
zeroAska <trotwoodray@gmail.com>
2019-06-15 17:50:43 -0400

    add code for costmap

--- config/optimize_graph_working.bash
+++ config/optimize_graph_working.bash
@@ -1,9 +1,10 @@
 ~/thirdparty/tensorflow/bazel-bin/tensorflow/tools/graph_transforms/transform_graph \
---in_graph='mobilenet_const_nclt_2019_06_11.pb' \
---out_graph='optimized_mobilenet_const_nclt_2019_06_11.pb' \
+--in_graph='mobilenet_nclt/optimized_mobilenet_frozen_nclt_bn_480.pb' \
+--out_graph='mobilenet_nclt/optimized_mobilenet_nclt_bn_2019_06_14.pb' \
 --inputs='network/input/Placeholder' \
 --outputs='network/output/ArgMax,network/upscore_8s/upscore8/upscore8/BiasAdd' \
 --transforms='
+fold_constants(ignore_errors=true)
 fold_old_batch_norms
 strip_unused_nodes
 '
